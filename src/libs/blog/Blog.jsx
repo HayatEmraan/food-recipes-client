58dce81e4e913ded1875e0db72e68682d9357d0f
hayatemraan <emraanhasanhayat@gmail.com>
2023-05-05 06:53:21 +0600

    made some changes

--- src/libs/blog/Blog.jsx
+++ src/libs/blog/Blog.jsx
@@ -1,14 +1,41 @@
-import React from 'react';
-import { useLoaderData } from 'react-router-dom';
+import { BookOpenIcon, BookmarkIcon } from "@heroicons/react/24/solid";
+import React, { useState } from "react";
+import { useLoaderData } from "react-router-dom";
 
 const Blog = () => {
-    const blog = useLoaderData();
-    console.log(blog);
-    return (
-        <div>
-            
+  const blog = useLoaderData();
+  const [bookMark, setBookmark] = useState(false);
+  return (
+    <div className="container mx-auto">
+      <div>
+        <img className="w-full h-64 rounded-md" src={blog.image} alt="" />
+        <div className="flex justify-between mt-3">
+          <div>
+            <p>{blog.publishedBy.name}</p>
+            <p>{blog.date}</p>
+          </div>
+          <div>
+            <button onClick={() => setBookmark(true)} disabled={bookMark}>
+              {bookMark ? (
+                <BookmarkIcon className="w-8 h-8" />
+              ) : (
+                <BookOpenIcon className="w-8 h-8" />
+              )}
+            </button>
+          </div>
         </div>
-    );
+        <p className="my-8 text-lg">{blog.description}</p>
+        {blog.features &&
+          blog.features.map((feature) => {
+            return (
+              <li className="my-3" key={feature}>
+                {feature}
+              </li>
+            );
+          })}
+      </div>
+    </div>
+  );
 };
 
 export default Blog;
